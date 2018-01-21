9ad4612e20abcf85a48169b5969d59d7d852f4bd
JesseBusman <info@jesbus.com>
2018-01-21 16:31:31 +0100

    The slider can now function, but it's still buggy.

--- input_type_is_slider.js
+++ input_type_is_slider.js
@@ -25,6 +25,7 @@ window.addEventListener("load", function(){
 					else if (key === "min") value = parseFloat(value);
 					else if (key === "max") value = parseFloat(value);
 					else if (key === "value") value = parseFloat(value);
+					else if (key === "width") value = parseFloat(value);
 					else continue;
 					slider.setAttribute(key, value);
 					slider[key] = value;
@@ -38,8 +39,15 @@ window.addEventListener("load", function(){
 					sliderKnob.style.backgroundColor = "black";
 					sliderKnob.style.position = "relative";
 					sliderKnob.style.top = "-20px";
+					sliderKnob.style.opacity = 0.5;
 				}
 				slider.appendChild(sliderKnob);
+				
+				if (!slider.hasOwnProperty("width"))
+				{
+					slider.width = 100;
+				}
+				slider.style.width = slider.width+"px";
 				
 				if (!slider.hasOwnProperty("min"))
 				{
@@ -77,17 +85,84 @@ window.addEventListener("load", function(){
 					slider._initValue = slider.min;
 				}
 				
+				var slideDistance = parseInt(slider.width - parseInt(sliderKnob.style.width));
+				
 				Object.defineProperty(slider, "value", {
 					get: function() { return slider._theValue; },
 					set: function(newValue)
 					{
+						console.log("Setting slider value to "+newValue);
 						newValue = parseFloat(newValue);
+						if (newValue < slider.min) newValue = slider.min;
+						if (newValue > slider.max) newValue = slider.max;
+						var oldValue = slider._theValue;
 						slider._theValue = newValue;
-						sliderKnob.style.left = ((newValue-min) / (max-min) * sliderWidth) + "px";
+						/*setTimeout(function(){
+							console.log("slider.clientWidth="+slider.clientWidth);
+							console.log("slider.offsetWidth="+slider.offsetWidth);
+							console.log("slider.getBoundingClientRect().width="+slider.getBoundingClientRect().width);
+						}, 1000);*/
+						sliderKnob.style.left = ((newValue-slider.min) / (slider.max-slider.min) * slideDistance) + "px";
+						console.log("knob now at: "+sliderKnob.style.left);
+						if (newValue != oldValue)
+						{
+							for (var i=0; i<slider._onChangeCallbacks.length; i++)
+							{
+								slider._onChangeCallbacks[i]();
+							}
+						}
+					}
+				});
+				slider._onChangeCallbacks = [];
+				slider.value = slider._initValue;
+				
+				var dragging = false;
+				var startDraggingMouseX = null;
+				var startDraggingValue = null;
+				
+				sliderKnob.addEventListener("mousedown", function(e){
+					startDraggingMouseX = e.clientX;
+					startDraggingValue = slider.value;
+					dragging = true;
+				});
+				window.addEventListener("mousemove", function(e){
+					if (dragging)
+					{
+						slider.value = startDraggingValue + (e.clientX - startDraggingMouseX) * (slider.max-slider.min) / slideDistance;
+					}
+					e.preventDefault();
+					return false;
+				});
+				document.addEventListener("mouseout", function(e){
+					if (dragging)
+					{
+						dragging = false;
+						console.log("Stopped dragging because of mouseout");
+						e.preventDefault();
+						return false;
+					}
+				});
+				document.addEventListener("mouseup", function(e){
+					if (dragging)
+					{
+						dragging = false;
+						console.log("Stopped dragging because of mouseup");
+						e.preventDefault();
+						return false;
 					}
 				});
 				
-				slider.value = slider._initValue;
+				slider.addEventListener = function(event, callback) // lol
+				{
+					if (event === "change")
+					{
+						
+					}
+					else
+					{
+						throw "Event '"+event+"' is not implemented by <input type='slider'>. Try 'change'.";
+					}
+				};
 			}
 			el.parentNode.replaceChild(slider, el);
 		}
